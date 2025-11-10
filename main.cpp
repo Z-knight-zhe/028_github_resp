870e1022ad9db37f5ea4be73b9a3aac3b5a6c250
zhangzhe <1139206613@qq.com>
2025-11-10 11:23:14 +0800

    [ADD]First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iostream>
+
+int main() {
+  printf("Hello, World!\n");
+  std::cout << "你好" << std::endl;
+  std::cout << std::endl;
+  std::cout << "不好bu" << std::endl;
+  return 0;
+}
